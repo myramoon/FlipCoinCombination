3e27365f31c6c4869e4e6a97b400b17e644d86e9
anamfazal <anamfazal94@gmail.com>
2020-08-22 18:24:30 +0530

    Refactors code to store doublet combinations and their percentages

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,28 +1,40 @@
-#!/usr/local/bin/bash -x
+#!/usr/local/bin/bash 
 
 #DICTIONARY
-declare -A flipdictionary  					    #Stores singlet combination
+declare -A flipdictionary  					    #Stores doublet combination
 
 #MAIN PROGRAM
 
 read -p "Enter number of times you want to flip: " fliptimes
-flipdictionary["H"]=0						    #initialize values stored in dictionary indices
-flipdictionary["T"]=0
+flipdictionary["HH"]=0						    #initialize values stored in dictionary indices
+flipdictionary["HT"]=0
+flipdictionary["TH"]=0
+flipdictionary["TT"]=0
 
 for (( count=0; count<$fliptimes; count++))			    #for loop to run multiple simulations
 do
-	flip_Result=$((RANDOM%2))				    #get flip result for current coin flip
+	coin1=$((RANDOM%2))					    #get flip result for COIN1
+	coin2=$((RANDOM%2))				    	    #get flip result for COIN2 
+	
+	case "$coin1$coin2" in				            #case statement to determine number of times each combination appeared				
+	
+		11)
+		flipdictionary["HH"]=$((${flipdictionary["HH"]} + 1 ));;
+		10)
+		flipdictionary["HT"]=$((${flipdictionary["HT"]} + 1 ));;
+		01)
+		flipdictionary["TH"]=$((${flipdictionary["TH"]} + 1 ));;
+		00)
+		flipdictionary["TT"]=$((${flipdictionary["TT"]} + 1 ));;
+	esac
 
-	if [[ $flip_Result -eq 1 ]]
-	then
-		flipdictionary["H"]=$((${flipdictionary["H"]} + 1)) #increment heads if flip result is 1
-	else 
-		flipdictionary["T"]=$((${flipdictionary["T"]} + 1)) #increment tails if flip result is 0
-	fi
 done
 
-singlet_percentage=`echo "scale=2; ${flipdictionary["H"]} / $fliptimes * 100" | bc -l |awk -F. '{print $1}'`  #Determine singlet percentage,display integral part
+flipdictionary["HH%"]=`echo "scale=2; ${flipdictionary["HH"]} / $fliptimes * 100" | bc -l |awk -F. '{print $1 "%"}'`  #Determine doublet percentage,display integral part
+flipdictionary["HT%"]=`echo "scale=2; ${flipdictionary["HT"]} / $fliptimes * 100" | bc -l |awk -F. '{print $1 "%"}'`
+flipdictionary["TH%"]=`echo "scale=2; ${flipdictionary["TH"]} / $fliptimes * 100" | bc -l |awk -F. '{print $1 "%"}'`
+flipdictionary["TT%"]=`echo "scale=2; ${flipdictionary["TT"]} / $fliptimes * 100" | bc -l |awk -F. '{print $1 "%"}'`
 
 echo ${!flipdictionary[@]}					    #Print results
 echo ${flipdictionary[@]}
-echo "Singlet percentage: " $singlet_percentage			    
+
