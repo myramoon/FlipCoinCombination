72dfa00100b835e3402d559afba54e050460c72e
anamfazal <anamfazal94@gmail.com>
2020-08-22 17:38:45 +0530

    Stores singlet combinations in dictionary and displays singlet percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,10 +1,28 @@
-#!/usr/local/bin/bash
+#!/usr/local/bin/bash -x
 
-flip_Result=$((RANDOM%2))                       #get flip result for current coin flip
+#DICTIONARY
+declare -A flipdictionary  					    #Stores singlet combination
 
-if [[ $flip_Result -eq 1 ]]
-then
-	echo "Result of flip: HEADS"
-else 
-	echo "Result of flip: TAILS"
-fi
+#MAIN PROGRAM
+
+read -p "Enter number of times you want to flip: " fliptimes
+flipdictionary["H"]=0						    #initialize values stored in dictionary indices
+flipdictionary["T"]=0
+
+for (( count=0; count<$fliptimes; count++))			    #for loop to run multiple simulations
+do
+	flip_Result=$((RANDOM%2))				    #get flip result for current coin flip
+
+	if [[ $flip_Result -eq 1 ]]
+	then
+		flipdictionary["H"]=$((${flipdictionary["H"]} + 1)) #increment heads if flip result is 1
+	else 
+		flipdictionary["T"]=$((${flipdictionary["T"]} + 1)) #increment tails if flip result is 0
+	fi
+done
+
+singlet_percentage=`echo "scale=2; ${flipdictionary["H"]} / $fliptimes * 100" | bc -l |awk -F. '{print $1}'`  #Determine singlet percentage,display integral part
+
+echo ${!flipdictionary[@]}					    #Print results
+echo ${flipdictionary[@]}
+echo "Singlet percentage: " $singlet_percentage			    
